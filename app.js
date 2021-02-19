cd65c536c78a735efcf8db61762175b253a6439c
Ca-rlos <carlos.eft.social@gmail.com>
2021-02-19 17:50:10 +0900

    Enabling CORS for front app usage

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 const indexRouter = require('./routes/index');
-
 const app = express();
 
 // view engine setup
@@ -14,6 +14,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
