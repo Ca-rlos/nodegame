055071c4b99ed517053cacd9444e71bbfa156dad
Ca-rlos <carlos.eft.social@gmail.com>
2021-02-22 22:03:59 +0900

    More Passport auth, no session for now

--- routes/index.js
+++ routes/index.js
@@ -11,30 +11,30 @@ router.get('/', function(req, res, next) {
 
 router.get('/player', passport.authenticate('local', {passReqToCallback: true, session: false, failureRedirect: '/'}), require('./player'));
 
-router.put('/player/item', require('./player'));
+router.put('/player/item', passport.authenticate('local', {passReqToCallback: true, session: false, failureRedirect: '/'}), require('./player'));
 
-router.delete('/player/item', require('./player'));
+router.delete('/player/item', passport.authenticate('local', {passReqToCallback: true, session: false, failureRedirect: '/'}), require('./player'));
 
-router.put('/player/zone', require('./player'));
+router.put('/player/zone', passport.authenticate('local', {passReqToCallback: true, session: false, failureRedirect: '/'}), require('./player'));
 
 // Enemy management.
 
-router.get('/enemy', require('./enemy'));
+router.get('/enemy', passport.authenticate('local', {passReqToCallback: true, session: false, failureRedirect: '/'}), require('./enemy'));
 
 // Item management.
 
-router.get('/item', require('./items'));
+router.get('/item', passport.authenticate('local', {passReqToCallback: true, session: false, failureRedirect: '/'}), require('./items'));
 
-router.get('/item/all', require('./items'));
+router.get('/item/all', passport.authenticate('local', {passReqToCallback: true, session: false, failureRedirect: '/'}), require('./items'));
 
 // Battle management.
 
-router.get('/battle', require('./battle'));
+router.get('/battle', passport.authenticate('local', {passReqToCallback: true, session: false, failureRedirect: '/'}), require('./battle'));
 
 // Location management.
 
-router.get('/location', require('./location'));
+router.get('/location', passport.authenticate('local', {passReqToCallback: true, session: false, failureRedirect: '/'}), require('./location'));
 
-router.get('/location/zone', require('./location'));
+router.get('/location/zone', passport.authenticate('local', {passReqToCallback: true, session: false, failureRedirect: '/'}), require('./location'));
 
 module.exports = router;
